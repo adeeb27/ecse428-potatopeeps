57226bb5eff10562c1a31906c840a85aea051a58
EBruchet <evan.bruchet@mail.mcgill.ca>
2019-02-12 11:49:59 -0500

    Added missing dependencies for the project, including the Spring-Data-Starter and Spring-Java-MySQL-Connector dependencies. Additionally added the username and password settings in the application.properties file for DB connection.

--- src/main/java/ca/mcgill/ecse428/potatopeeps/config/DatabaseLoader.java
+++ src/main/java/ca/mcgill/ecse428/potatopeeps/config/DatabaseLoader.java
@@ -16,6 +16,9 @@ public class DatabaseLoader implements CommandLineRunner {
         this.menuItemRepository = menuItemRepository;
     }
 
+
+    // Objects generated and saved within this run method will be added and
+    // stored directly to the connected database.
     @Override
     public void run(String... strings) throws Exception {
         this.menuItemRepository.save(new MenuItem("Calamari", 9.99, 20, "A generous portion of fresh, tender squid served in a blend of Italian tomatoes, virgin olive oil and spices."));
