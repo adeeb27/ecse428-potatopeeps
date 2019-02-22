8d021c9c64db72c717413bac0037d1fbb5b599e2
bilasa <bilasa999@gmail.com>
2019-02-21 19:57:31 -0500

    Implement code to add menu items and tags to DB

--- src/main/java/ca/mcgill/ecse428/potatopeeps/config/DatabaseLoader.java
+++ src/main/java/ca/mcgill/ecse428/potatopeeps/config/DatabaseLoader.java
@@ -32,13 +32,63 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         try{
-            this.menuItemRepository.save(new MenuItem("Calamari", 9.99, 20, "A generous portion of fresh, tender squid served in a blend of Italian tomatoes, virgin olive oil and spices."));
-            this.menuItemRepository.save(new MenuItem("Snap Peas", 3.50, 100, "Roasted snap peas with sea salt."));
-            this.menuItemRepository.save(new MenuItem("Popcorn Shrimp", 4.99, 100, "Deep fried small shrimp"));
-            this.menuItemRepository.save(new MenuItem("Chicken Curry", 10.99, 100, "Mild Chicken Curry on rice"));
-            this.tagRepository.save(new Tag("Seafood"));
-            this.tagRepository.save(new Tag("Vegetarian"));
-            this.tagRepository.save(new Tag("Meat"));
+            // CLEAR EXISTING TABLES
+            Iterable<MenuItem> oldItems = menuItemRepository.findAll();
+            for(MenuItem oldItem: oldItems){
+                menuItemRepository.delete(oldItem);
+            }
+            Iterable<Tag> oldTags = tagRepository.findAll();
+            for(Tag oldTag : oldTags){
+                tagRepository.delete(oldTag);
+            }
+
+            // FILL MENU_ITEM TABLE
+            MenuItem[] menuItems = new MenuItem[26];
+            menuItems[0] = new MenuItem("snap peas", 3.5, 100, "roasted snap peas with sea salt");
+            menuItems[1] = new MenuItem("spring roll", 4.0, 100, "--");
+            menuItems[2] = new MenuItem("imperial roll", 4.0, 100, "--");
+            menuItems[3] = new MenuItem("popcorn shrimp", 5.0, 100, "deep fried small shrimp");
+            menuItems[4] = new MenuItem("tempura vegetables", 3.0, 100, "vegetables deep fried in tempura");
+            menuItems[5] = new MenuItem("chicken curry", 11.0, 100, "mild chicken curry on rice");
+            menuItems[6] = new MenuItem("vegetable curry", 10.0, 100, "moderately spicy vegetable curry on rice");
+            menuItems[7] = new MenuItem("lamb curry", 12.0, 100, "spicy lamb curry on rice");
+            menuItems[8] = new MenuItem("pork ramen", 12.0, 100, "ramen in pork broth");
+            menuItems[9] = new MenuItem("chicken ramen", 11.0, 100, "ramen in chicken broth");
+            menuItems[10] = new MenuItem("vegetarian ramen", 10.0, 100, "miso ramen, vegetable broth");
+            menuItems[11] = new MenuItem("spicy salmon roll", 6.0, 100, "spicy salmon rolls, 6 pcs");
+            menuItems[12] = new MenuItem("california roll", 5.0, 100, "cucumber, imitation crab and avocado roll, 6 pcs");
+            menuItems[13] = new MenuItem("dynamite roll", 6.0, 100, "shrimp tempura, avocado, cucumber, masago roll, 4 pcs");
+            menuItems[14] = new MenuItem("salmon sashimi", 6.0, 100, "fresh, raw salmon piece");
+            menuItems[15] = new MenuItem("tuna sashimi", 10.0, 100, "fresh, raw tuna piece");
+            menuItems[16] = new MenuItem("white tuna sashimi", 12.0, 100, "fresh, raw white tuna piece");
+            menuItems[17] = new MenuItem("deep fried banana piece", 2.0, 100, "deep fried banana piece covered with syrup");
+            menuItems[18] = new MenuItem("green tea ice cream", 2.0, 100, "green tea ice cream");
+            menuItems[19] = new MenuItem("mango ice cream", 2.0, 100, "mango ice cream");
+            menuItems[20] = new MenuItem("coke", 2.0, 100, "standard coke brand soft drink, served in a cup with ice");
+            menuItems[21] = new MenuItem("sprite", 2.0, 100, "standard sprite brand soft drink, served in a cup with ice");
+            menuItems[22] = new MenuItem("crush", 2.0, 100, "standard crush brand soft drink, served in a cup with ice");
+            menuItems[23] = new MenuItem("beer", 4.0, 100, "microbrewed local beer");
+            menuItems[24] = new MenuItem("red wine", 6.0, 100, "red wine, 15 years");
+            menuItems[25] = new MenuItem("white wine", 6.0, 100, "white wine, 15 years");
+            for (MenuItem menuItem : menuItems) {
+                this.menuItemRepository.save(menuItem);
+            }
+
+            // FILL TAGS TABLE
+            Tag[] tags = new Tag[7];
+            tags[0] = new Tag("appetizer");
+            tags[1] = new Tag("main meal");
+            tags[2] = new Tag("soup");
+            tags[3] = new Tag("dessert");
+            tags[4] = new Tag("drink");
+            tags[5] = new Tag("vegetarian");
+            tags[6] = new Tag("spicy");
+            for (Tag tag : tags) {
+                this.tagRepository.save(tag);
+            }
+
+            // TO-DO: ASSIGN TAGS TO MENU ITEMS
+
         } catch(Exception e){
             // Already exists in your database.
         }
