30c758ff1149ad2e21e641914aee3295b0655fc5
EBruchet <evan.bruchet@mail.mcgill.ca>
2019-03-08 16:28:59 -0500

    MenuItemList now displays correctly with proper styling and has been placed within its own respective component.

--- src/main/js/components/Manager.js
+++ src/main/js/components/Manager.js
@@ -1,4 +1,9 @@
 import React from "react";
+// const root = '/api';
+const root = "../api";
+import client from "../client";
+import follow from "../follow";
+
 import "../../resources/static/css/manager.css";
 import "../../resources/static/css/external/bootstrap.min.css";
 
@@ -13,95 +18,94 @@ import {faTrash} from "@fortawesome/free-solid-svg-icons";
  */
 
 export class Manager extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {menuItems: [], tags: [], attributes: [], pagesize: 2, links: {}};
-    //     this.updatePageSize = this.updatePageSize.bind(this);
-    //     this.onCreate = this.onCreate.bind(this);
-    //     this.onDelete = this.onDelete.bind(this);
-    //     this.onNavigate = this.onNavigate.bind(this);
-    // }
-    //
-    // loadFromServer(pageSize) {
-    //     follow(client, root, [
-    //         {rel: 'menuItems', params: {size: pageSize}}]
-    //     ).then(menuItemCollection => {
-    //         return client({
-    //             method: 'GET',
-    //             path: menuItemCollection.entity._links.profile.href,
-    //             headers: {'Accept': 'application/schema+json'}
-    //         }).then(schema => {
-    //             this.schema = schema.entity;
-    //             return menuItemCollection;
-    //         });
-    //     }).done(menuItemCollection => {
-    //         this.setState({
-    //             menuItems: menuItemCollection.entity._embedded.menuItems,
-    //             attributes: Object.keys(this.schema.properties).filter(attribute => attribute !== 'tags' && attribute !== 'orders'),
-    //             pageSize: pageSize,
-    //             links: menuItemCollection.entity._links});
-    //     });
-    // }
-    //
-    // onCreate(newMenuItem) {
-    //     follow(client, root, ['menuItems']).then(menuItemCollection => {
-    //         return client({
-    //             method: 'POST',
-    //             path: menuItemCollection.entity._links.self.href,
-    //             entity: newMenuItem,
-    //             headers: {'Content-Type': 'application/json'}
-    //         })
-    //     }).then(response => {
-    //         return follow(client, root, [
-    //             {rel: 'menuItems', params: {'size': this.state.pageSize}}]);
-    //     }).done(response => {
-    //         if (typeof response.entity._links.last !== "undefined") {
-    //             this.onNavigate(response.entity._links.last.href);
-    //         } else {
-    //             this.onNavigate(response.entity._links.self.href);
-    //         }
-    //     });
-    // }
-    // // end::create[]
-    //
-    // // tag::delete[]
-    // onDelete(menuItem) {
-    //     client({method: 'DELETE', path: menuItem._links.self.href}).done(response => {
-    //         this.loadFromServer(this.state.pageSize);
-    //     });
-    // }
-    // // end::delete[]
-    //
-    // // tag::navigate[]
-    // onNavigate(navUri) {
-    //     client({method: 'GET', path: navUri}).done(menuItemCollection => {
-    //         this.setState({
-    //             menuItems: menuItemCollection.entity._embedded.menuItems,
-    //             attributes: this.state.attributes,
-    //             pageSize: this.state.pageSize,
-    //             links: menuItemCollection.entity._links
-    //         });
-    //     });
-    // }
-    // // end::navigate[]
-    //
-    // // tag::update-page-size[]
-    // updatePageSize(pageSize) {
-    //     if (pageSize !== this.state.pageSize) {
-    //         this.loadFromServer(pageSize);
-    //     }
-    // }
-    // // end::update-page-size[]
-    //
-    // // tag::follow-1[]
-    // componentDidMount() {
-    //     this.loadFromServer(this.state.pageSize);
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {menuItems: [], tags: [], attributes: [], pagesize: 2, links: {}};
+        this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.onNavigate = this.onNavigate.bind(this);
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'menuItems', params: {size: pageSize}}]
+        ).then(menuItemCollection => {
+            return client({
+                method: 'GET',
+                path: menuItemCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return menuItemCollection;
+            });
+        }).done(menuItemCollection => {
+            this.setState({
+                menuItems: menuItemCollection.entity._embedded.menuItems,
+                attributes: Object.keys(this.schema.properties).filter(attribute => attribute !== 'tags' && attribute !== 'orders'),
+                pageSize: pageSize,
+                links: menuItemCollection.entity._links});
+        });
+    }
+
+    onCreate(newMenuItem) {
+        follow(client, root, ['menuItems']).then(menuItemCollection => {
+            return client({
+                method: 'POST',
+                path: menuItemCollection.entity._links.self.href,
+                entity: newMenuItem,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'menuItems', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            if (typeof response.entity._links.last !== "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+    // end::create[]
+
+    // tag::delete[]
+    onDelete(menuItem) {
+        client({method: 'DELETE', path: menuItem._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+    // end::delete[]
+
+    // tag::navigate[]
+    onNavigate(navUri) {
+        client({method: 'GET', path: navUri}).done(menuItemCollection => {
+            this.setState({
+                menuItems: menuItemCollection.entity._embedded.menuItems,
+                attributes: this.state.attributes,
+                pageSize: this.state.pageSize,
+                links: menuItemCollection.entity._links
+            });
+        });
+    }
+    // end::navigate[]
+
+    // tag::update-page-size[]
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
+    }
+    // end::update-page-size[]
+
+    // tag::follow-1[]
+    componentDidMount() {
+        this.loadFromServer(this.state.pageSize);
+    }
 
     render() {
         return (
             <div className="page manager-page">
-
                 <div id="header" className="container-fluid">
                     <div className="row">
                         <div className="col-9">
@@ -119,7 +123,6 @@ export class Manager extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="dropdown-divider" />
                     <hr/>
                     <div className="row">
                         <div className="col-3">
@@ -132,37 +135,15 @@ export class Manager extends React.Component {
                     </div>
                 </div>
 
-
                 <div id="main-table-container" className="container-fluid">
                     <div className="row">
                         <div className="col-9">
-                            <div id="menu-items" className="table-responsive">
-                                <table id="main-table" className="table table-striped">
-                                    <tr>
-                                        <th>Category</th>
-                                        <th>Name</th>
-                                        <th>Description</th>
-                                        <th>Price</th>
-                                        <th>Quantity</th>
-                                        <th />
-                                        <th />
-                                    </tr>
-                                    <tr>
-                                        <td>appetizer vegetarian</td>
-                                        <td>snap peas</td>
-                                        <td>roasted snap peas with sea salt</td>
-                                        <td>9.99</td>
-                                        <td>500</td>
-                                        <td>
-                                            <button type="button" className="btn btn-primary">Edit</button>
-                                        </td>
-                                        <td>
-                                            <FontAwesomeIcon icon={faTrash}/>
-                                        </td>
-                                    </tr>
-                                </table>
-
-                            </div>
+                            <ManagerMenuItemList menuItems={this.state.menuItems}
+                                                 links = {this.state.links}
+                                                 pageSize = {this.state.pageSize}
+                                                 onNavigate = {this.onNavigate}
+                                                 onDelete = {this.onDelete}
+                                                 updatePageSize = {this.updatePageSize}/>
                         </div>
                         <div className="col-3">
                             <div className="container-fluid">
@@ -233,6 +214,102 @@ export class Manager extends React.Component {
 }
 
 /***
+ * Holds list of menuItems
+ * TODO: integrate UI of Manager
+ * NOTE: Renamed from MenuItemList
+ */
+class ManagerMenuItemList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // this.handleNavFirst = this.handleNavFirst.bind(this);
+        // this.handleNavPrev = this.handleNavPrev.bind(this);
+        // this.handleNavNext = this.handleNavNext.bind(this);
+        // this.handleNavLast = this.handleNavLast.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    // tag::handle-page-size-updates[]
+    handleInput(e) {
+        e.preventDefault();
+        const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+        if (/^[0-9]+$/.test(pageSize)) {
+            this.props.updatePageSize(pageSize);
+        } else {
+            ReactDOM.findDOMNode(this.refs.pageSize).value =
+                pageSize.substring(0, pageSize.length - 1);
+        }
+    }
+    // end::handle-page-size-updates[]
+
+    // tag::handle-nav[]
+    // handleNavFirst(e){
+    //     e.preventDefault();
+    //     this.props.onNavigate(this.props.links.first.href);
+    // }
+    //
+    // handleNavPrev(e) {
+    //     e.preventDefault();
+    //     this.props.onNavigate(this.props.links.prev.href);
+    // }
+    //
+    // handleNavNext(e) {
+    //     e.preventDefault();
+    //     this.props.onNavigate(this.props.links.next.href);
+    // }
+    //
+    // handleNavLast(e) {
+    //     e.preventDefault();
+    //     this.props.onNavigate(this.props.links.last.href);
+    // }
+    // end::handle-nav[]
+
+    // tag::menuItem-list-render[]
+    render() {
+        const menuItems = this.props.menuItems.map(menuItem =>
+            <MenuItem key={menuItem._links.self.href} menuItem={menuItem} onDelete={this.props.onDelete}/>
+        );
+
+        // const navLinks = [];
+        // if ("first" in this.props.links) {
+        //     navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+        // }
+        // if ("prev" in this.props.links) {
+        //     navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        // }
+        // if ("next" in this.props.links) {
+        //     navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        // }
+        // if ("last" in this.props.links) {
+        //     navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        // }
+
+        return (
+            <div id="menu-items" className="table-responsive">
+                {/*<input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>*/}
+                <table id="main-table" className="table table-striped">
+                    <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th />
+                        <th />
+                    </tr>
+                    {menuItems}
+                    </tbody>
+                </table>
+                {/*<div>*/}
+                {/*{navLinks}*/}
+                {/*</div>*/}
+            </div>
+        )
+    }
+    // end::menuItem-list-render[]
+}
+
+/***
  * Holds the code for a specific menu item
  *
  */
@@ -252,111 +329,25 @@ class MenuItem extends React.Component {
         return (
             <tr>
                 <td>{this.props.menuItem.name}</td>
+                <td>{this.props.menuItem.description}</td>
                 <td>{this.props.menuItem.price}</td>
                 <td>{this.props.menuItem.inventory}</td>
-                <td>{this.props.menuItem.description}</td>
                 <td>
-                    <button onClick={this.handleDelete}>Delete</button>
+                    <button type="button" className="btn btn-warning">
+                        Edit
+                    </button>
+                </td>
+                <td>
+                    <button className="btn btn-danger" onClick={this.handleDelete}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </button>
                 </td>
             </tr>
         )
     }
 }
 
-/***
- * Holds list of menuItems
- * TODO: integrate UI of Manager
- * NOTE: Renamed from MenuItemList
- */
-class ManagerMenu extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.handleNavFirst = this.handleNavFirst.bind(this);
-        this.handleNavPrev = this.handleNavPrev.bind(this);
-        this.handleNavNext = this.handleNavNext.bind(this);
-        this.handleNavLast = this.handleNavLast.bind(this);
-        this.handleInput = this.handleInput.bind(this);
-    }
-
-    // tag::handle-page-size-updates[]
-    handleInput(e) {
-        e.preventDefault();
-        const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
-        if (/^[0-9]+$/.test(pageSize)) {
-            this.props.updatePageSize(pageSize);
-        } else {
-            ReactDOM.findDOMNode(this.refs.pageSize).value =
-                pageSize.substring(0, pageSize.length - 1);
-        }
-    }
-    // end::handle-page-size-updates[]
-
-    // tag::handle-nav[]
-    handleNavFirst(e){
-        e.preventDefault();
-        this.props.onNavigate(this.props.links.first.href);
-    }
-
-    handleNavPrev(e) {
-        e.preventDefault();
-        this.props.onNavigate(this.props.links.prev.href);
-    }
-
-    handleNavNext(e) {
-        e.preventDefault();
-        this.props.onNavigate(this.props.links.next.href);
-    }
-
-    handleNavLast(e) {
-        e.preventDefault();
-        this.props.onNavigate(this.props.links.last.href);
-    }
-    // end::handle-nav[]
-
-    // tag::menuItem-list-render[]
-    render() {
-        const menuItems = this.props.menuItems.map(menuItem =>
-            <MenuItem key={menuItem._links.self.href} menuItem={menuItem} onDelete={this.props.onDelete}/>
-        );
-
-        const navLinks = [];
-        if ("first" in this.props.links) {
-            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
-        }
-        if ("prev" in this.props.links) {
-            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
-        }
-        if ("next" in this.props.links) {
-            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
-        }
-        if ("last" in this.props.links) {
-            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
-        }
-
-        return (
-            <div>
-                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
-                <table>
-                    <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Inventory</th>
-                        <th>Description</th>
-                        <th></th>
-                    </tr>
-                    {menuItems}
-                    </tbody>
-                </table>
-                <div>
-                    {navLinks}
-                </div>
-            </div>
-        )
-    }
-    // end::menuItem-list-render[]
-}
 
 /**
  * Holds the code related to creating menu items.
