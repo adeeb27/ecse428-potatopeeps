1d61fa6df2fecaefd2264b654dfce1b7dc1a50b3
bilasa <bilasa999@gmail.com>
2019-03-17 22:49:24 -0400

    put select table number page in JS file with navigation

--- src/main/js/components/Customer.js
+++ src/main/js/components/Customer.js
@@ -28,36 +28,33 @@ export class Customer extends React.Component {
     }
 
     goToCustomerLanding(e) {
-        e.preventDefault()
-        return (
-            <CustomerLanding/>
-        )
+        e.preventDefault();
+        this.props.history.push('/CustomerLanding');
     }
 
     render() {
         return (
-            <div>
+            <div id="main-stn" className={"page main-stn"}>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width,initial-scale=1" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                <link rel="stylesheet" id="style-css" href="../../resources/static/css/select_table_num.css" type="text/css" media="all"/>
                 <title>Table number selection</title>
-                <div className="background">
-                    <div className="shadow">
+                <div className="background-stn">
+                    <div className="shadow-stn">
                     </div>
-                    <div className="content">
-                        <div className="h-item">
-                            <h2>Please select a table number</h2>
+                    <div className="content-stn">
+                        <div className="h-item-stn">
+                            <h2 className="h2-stn">Please select a table number</h2>
                         </div>
                         <div className="table">
-                            <select className="tableID">
+                            <select className="tableID-stn">
                                 <option value="table1">Table No.1</option>
                                 <option value="table2">Table No.2</option>
                                 <option value="table3">Table No.3</option>
                             </select>
                         </div>
                         <div className="submit-button">
-                            <button type="button" className="submit" onClick={this.goToCustomerLanding}>submit</button>
+                            <button type="button" className="submit-stn" onClick={this.goToCustomerLanding}>submit</button>
                         </div>
                     </div>
                 </div>
