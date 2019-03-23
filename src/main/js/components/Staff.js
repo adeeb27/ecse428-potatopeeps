2c002bf29b8b2e025b25e48d91bf828d335f6e29
anqili426 <mollisterkl@outlook.com>
2019-03-23 15:00:32 -0400

    Add requests page.

--- src/main/js/components/Staff.js
+++ src/main/js/components/Staff.js
@@ -106,9 +106,117 @@ export class StaffRequests extends React.Component {
     render() {
         return (
             <div>
-
+                <div id="wrapper">
+                    <main className="main-wrapper">
+                        <header className="detail full">
+                            <a className="back" href="#" data-transition="slide-from-top" />
+                            <section>
+                                <h1 className="category-title">Requests</h1>
+                                <h3 className="page-badge">Bill and more</h3>
+                            </section>
+                        </header>
+                        <div className="content-wrap full-width">
+                            <div className="gridViewContainer">
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/4.jpg" />
+                                    <div className="overlay billReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Bill Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="View Detail" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>View Detail</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/3.jpg" />
+                                    <div className="overlay serviceReq">
+                                        <div className="text">Table 15</div>
+                                        <div className="text">Service Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="Answer" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>Answer</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/2.jpg" />
+                                    <div className="overlay answeringReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Service Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="Clear request" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>OK</i>
+                                            </button>
+                                        </div>
+                                        <div className="answerStatus">***Answering***</div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/5.jpg" />
+                                    <div className="overlay billReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Bill Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="View Detail" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>View Detail</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/6.jpg" />
+                                    <div className="overlay billReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Bill Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="View Detail" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>View Detail</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/4.jpg" />
+                                    <div className="overlay billReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Bill Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="View Detail" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>View Detail</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="gridViewItem">
+                                    <img className="itemImage" draggable="false" src="./img/2.jpg" />
+                                    <div className="overlay billReq">
+                                        <div className="text">Table 23</div>
+                                        <div className="text">Bill Request</div>
+                                        <div style={{display: 'flex', justifyContent: 'center'}}>
+                                            <button title="View Detail" className="view-detail-button">
+                                                <i className="view-order" style={{fontSize: '20px'}}>View Detail</i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <footer>
+                                <div className="signature">
+                                    <h6>Sushi</h6>
+                                    <h5>PotatoPeeps</h5>
+                                </div>
+                            </footer>
+                        </div>
+                    </main>
+                </div>
+                <a id="back-to-top" href="#">
+                    <i className="icon bg icon-UpArrow" />
+                </a>
             </div>
-        );
+    );
     }
 }
 
