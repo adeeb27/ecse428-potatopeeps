528326343d4e3eef298086cafbba4cf1aa442612
dishiGit <43579428+dishiGit@users.noreply.github.com>
2019-03-24 19:37:44 -0400

    Staff updated landing page and notifications (#29) with implemented refreshing load from servers
    * Add staff landing page
    
    * Change landing page appearance
    
    * Updated Staff.js with button on click functionality, with various other fixes - elaborated upon in PR comments.
    
    * Add requests page.
    
    * attempt redirect
    
    * Dynamically load requests on page
    
    * Fix navigation issue for returning to "Staff Landing Page"
    
    * Add notification bell
    
    * Fix a bug about notification count
    
    * add reload function to update the numebr of reqs and orders
    
    * ammended staff page notifications, requires fixing of routing
    
    * modified redirection to allow for propper reloading from server as well as added class to fix css styling

--- src/main/js/components/Staff.js
+++ src/main/js/components/Staff.js
@@ -1,7 +1,5 @@
 /** ----- NPM PACKAGE IMPORTS -----**/
 import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faShoppingCart, faDollarSign, faUser, faBell} from "@fortawesome/free-solid-svg-icons";
 
 /** ----- COMPONENT IMPORTS -----**/
 
@@ -16,6 +14,22 @@ import "../../resources/static/css/staff.css";
  */
 
 export class Staff extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {selectedView: 'Staff'};
+        this.reloader = this.reloader.bind(this);
+    }
+
+    reloader(){
+        this.props.loadResourceFromServer('diningSessions', this.state.pageSize);
+        this.props.loadResourceFromServer('orders', this.state.pageSize);
+    }
+
+    componentDidMount() {
+        this.props.loadResourceFromServer('diningSessions', this.state.pageSize);
+        setInterval(this.reloader, 30000);
+    }
+
     render() {
         return (
             <div className={"page staff-page"}>
@@ -25,13 +39,14 @@ export class Staff extends React.Component {
                               orderAttributes={this.props.orderAttributes}
                               diningSessions={this.props.diningSessions}
                               diningSessionsAttributes={this.props.diningSessionAttributes}
+                              filterDiningSessionList={this.props.filterDiningSessionList}
                               selectedView={this.props.selectedView}/>
             </div>
         )
     }
 }
 
-class StaffLanding extends React.Component {
+export class StaffLanding extends React.Component {
     constructor(props) {
         super(props);
         this.handleClickOrders = this.handleClickOrders.bind(this);
@@ -46,6 +61,7 @@ class StaffLanding extends React.Component {
             state: {onUpdate: this.props.onUpdate,
                     diningSessions: this.props.diningSessions,
                     diningSessionAttributes: this.props.diningSessionAttributes,
+                    filterDiningSessionList: this.props.filterDiningSessionList,
                     selectedView: this.props.selectedView}
         })
     }
@@ -56,13 +72,16 @@ class StaffLanding extends React.Component {
         this.props.history.push({
             pathname: '/staff-orders',
             state: {onUpdate: this.props.onUpdate,
-                    orders: this.props.orders,
-                    orderAttributes: this.props.orderAttributes,
-                    selectedView: this.props.selectedView}
+                orders: this.props.orders,
+                orderAttributes: this.props.orderAttributes,
+                selectedView: this.props.selectedView}
         })
     }
 
     render() {
+        let numBR = this.props.filterDiningSessionList("br_status").length;
+        let numSR = this.props.filterDiningSessionList("sr_status").length;
+        let numOR = this.props.orders.length;
         return (
             <div>
                 <title>Staff Landing Page</title>
@@ -77,9 +96,16 @@ class StaffLanding extends React.Component {
                     <div className="staff-overlay">
                         <div className="staff-nav-btn-wrapper">
                             <button className="staff-nav-btn" onClick={this.handleClickRequests}>All Requests</button>
+                           <div >
+                               <span className="staff-notification">Service Requests: {numSR}</span>
+                                <span className="staff-notification staff-request-notification">Bill Requests: {numBR}</span>
+                           </div>
                         </div>
                         <div className="staff-nav-btn-wrapper">
                             <button className="staff-nav-btn" onClick={this.handleClickOrders}>All Orders</button>
+                            <div >
+                                <span className="staff-notification">Orders: {numOR}</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -99,17 +125,112 @@ class StaffLanding extends React.Component {
  * to the Staff component above.
  */
 export class StaffRequests extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // this.state = {clicked: false};
+        this.handleBackClick = this.handleBackClick.bind(this);
+    }
+
+
+    handleBackClick(e) {
+        e.preventDefault();
+        this.props.history.push({
+            pathname: '/staff'
+        });
+    }
+
+    render() {
+        const StaffBillRequestItems = this.props.location.state.filterDiningSessionList("br_status").map(bill_request =>
+            <StaffBillRequestItem key={bill_request.entity._links.self.href}
+                                  bill_request={bill_request}
+            />);
+
+
+        const StaffServiceRequestItems = this.props.location.state.filterDiningSessionList("sr_status").map(service_request =>
+            <StaffServiceRequestItem key={service_request.entity._links.self.href}
+                                     service_request={service_request}
+            />);
+
+        return (
+            <div className="page staff-request-page">
+                <div id="wrapper">
+                    <main className="main-wrapper">
+                        <header className="detail full">
+                            <a className="back" onClick={this.handleBackClick} data-transition="slide-from-top" />
+                            <section>
+                                <h1 className="category-title">Requests</h1>
+                                <h3 className="page-badge">Bill and more</h3>
+                            </section>
+                        </header>
+                        <div className="content-wrap full-width">
+                            <div className="gridViewContainer">
+                                {StaffBillRequestItems}
+                                {StaffServiceRequestItems}
+                            </div>
+                            <footer>
+                                <div className="signature">
+                                    <h6>Sushi</h6>
+                                    <h5>PotatoPeeps</h5>
+                                </div>
+                            </footer>
+                        </div>
+                    </main>
+                </div>
+            </div>
+        );
+    }
+}
+
+export class StaffBillRequestItem extends React.Component {
     constructor(props) {
         super(props);
     }
 
+
     render() {
         return (
-            <div>
-
+            <div className="gridViewItem">
+                <img className="itemImage" draggable="false" src="./img/2.jpg" />
+                <div className="overlay billReq">
+                    <div className="text">Table {this.props.bill_request.entity.tableNumber}</div>
+                    <div className="text">Bill Request</div>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <button title="View Detail" className="view-detail-button">
+                            <i className="view-order" style={{fontSize: '20px'}}>Answer</i>
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
+
+}
+
+export class StaffServiceRequestItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div className="gridViewItem">
+                <img className="itemImage" draggable="false" src="./img/4.jpg" />
+                <div className="overlay serviceReq">
+                    <div className="text">Table {this.props.service_request.entity.tableNumber}</div>
+                    <div className="text">Service Request</div>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                        <button title="View Detail" className="view-detail-button">
+                            <i className="view-order" style={{fontSize: '20px'}}>Answer</i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+
 }
 
 
